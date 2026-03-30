81f1b189f3392af7ceb3dc482357df3f3e9481d0
leidy-correa <lorena99lozano@email.com>
2026-03-29 23:53:46 -0500

    Se movieron las validaciones del form a las clases

--- estudiante/Form1.Designer.cs
+++ estudiante/Form1.Designer.cs
@@ -36,80 +36,158 @@
             lbltxtNombre = new Label();
             lblEdad = new Label();
             lblPromedio = new Label();
+            label1 = new Label();
+            btnBuscar = new Button();
+            lblBuscar = new Label();
+            txtBuscar = new TextBox();
+            lblResultado = new Label();
+            label2 = new Label();
             SuspendLayout();
             // 
             // txtNombre
             // 
-            txtNombre.Location = new Point(213, 137);
+            txtNombre.Font = new Font("Cambria", 12F);
+            txtNombre.Location = new Point(170, 75);
             txtNombre.Name = "txtNombre";
-            txtNombre.Size = new Size(105, 23);
+            txtNombre.Size = new Size(105, 26);
             txtNombre.TabIndex = 0;
             // 
             // txtEdad
             // 
-            txtEdad.Location = new Point(213, 177);
+            txtEdad.Font = new Font("Cambria", 12F);
+            txtEdad.Location = new Point(170, 117);
             txtEdad.Name = "txtEdad";
-            txtEdad.Size = new Size(105, 23);
+            txtEdad.Size = new Size(105, 26);
             txtEdad.TabIndex = 1;
             // 
             // txtPromedio
             // 
-            txtPromedio.Location = new Point(213, 223);
+            txtPromedio.Font = new Font("Cambria", 12F);
+            txtPromedio.Location = new Point(170, 162);
             txtPromedio.Name = "txtPromedio";
-            txtPromedio.Size = new Size(105, 23);
+            txtPromedio.Size = new Size(105, 26);
             txtPromedio.TabIndex = 2;
             // 
             // btnAgregar
             // 
-            btnAgregar.Location = new Point(328, 328);
+            btnAgregar.Font = new Font("Cambria", 12F);
+            btnAgregar.Location = new Point(102, 206);
             btnAgregar.Name = "btnAgregar";
-            btnAgregar.Size = new Size(114, 44);
+            btnAgregar.Size = new Size(153, 44);
             btnAgregar.TabIndex = 3;
-            btnAgregar.Text = "Agregar";
+            btnAgregar.Text = "Agregar Estudiante";
             btnAgregar.UseVisualStyleBackColor = true;
             btnAgregar.Click += btnAgregar_Click;
             // 
             // listBoxEstudiantes
             // 
+            listBoxEstudiantes.Font = new Font("Cambria", 12F);
             listBoxEstudiantes.FormattingEnabled = true;
-            listBoxEstudiantes.ItemHeight = 15;
-            listBoxEstudiantes.Location = new Point(426, 101);
+            listBoxEstudiantes.ItemHeight = 19;
+            listBoxEstudiantes.Location = new Point(318, 75);
             listBoxEstudiantes.Name = "listBoxEstudiantes";
-            listBoxEstudiantes.Size = new Size(250, 184);
+            listBoxEstudiantes.Size = new Size(430, 175);
             listBoxEstudiantes.TabIndex = 4;
             // 
             // lbltxtNombre
             // 
             lbltxtNombre.AutoSize = true;
-            lbltxtNombre.Location = new Point(145, 137);
+            lbltxtNombre.Font = new Font("Cambria", 12F);
+            lbltxtNombre.Location = new Point(79, 82);
             lbltxtNombre.Name = "lbltxtNombre";
-            lbltxtNombre.Size = new Size(57, 15);
+            lbltxtNombre.Size = new Size(74, 19);
             lbltxtNombre.TabIndex = 5;
             lbltxtNombre.Text = "Nombre: ";
             // 
             // lblEdad
             // 
             lblEdad.AutoSize = true;
-            lblEdad.Location = new Point(145, 177);
+            lblEdad.Font = new Font("Cambria", 12F);
+            lblEdad.Location = new Point(79, 124);
             lblEdad.Name = "lblEdad";
-            lblEdad.Size = new Size(39, 15);
+            lblEdad.Size = new Size(52, 19);
             lblEdad.TabIndex = 6;
             lblEdad.Text = "Edad: ";
             // 
             // lblPromedio
             // 
             lblPromedio.AutoSize = true;
-            lblPromedio.Location = new Point(145, 231);
+            lblPromedio.Font = new Font("Cambria", 12F);
+            lblPromedio.Location = new Point(79, 169);
             lblPromedio.Name = "lblPromedio";
-            lblPromedio.Size = new Size(65, 15);
+            lblPromedio.Size = new Size(85, 19);
             lblPromedio.TabIndex = 7;
             lblPromedio.Text = "Promedio: ";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Cambria", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(258, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(223, 25);
+            label1.TabIndex = 8;
+            label1.Text = "Registro de estudiantes";
+            // 
+            // btnBuscar
+            // 
+            btnBuscar.Font = new Font("Cambria", 12F);
+            btnBuscar.Location = new Point(295, 323);
+            btnBuscar.Name = "btnBuscar";
+            btnBuscar.Size = new Size(97, 26);
+            btnBuscar.TabIndex = 9;
+            btnBuscar.Text = "Buscar";
+            btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // lblBuscar
+            // 
+            lblBuscar.AutoSize = true;
+            lblBuscar.Font = new Font("Cambria", 12F);
+            lblBuscar.Location = new Point(20, 326);
+            lblBuscar.Name = "lblBuscar";
+            lblBuscar.Size = new Size(149, 19);
+            lblBuscar.TabIndex = 10;
+            lblBuscar.Text = "Buscar por nombre:";
+            // 
+            // txtBuscar
+            // 
+            txtBuscar.Font = new Font("Cambria", 12F);
+            txtBuscar.Location = new Point(175, 323);
+            txtBuscar.Name = "txtBuscar";
+            txtBuscar.Size = new Size(100, 26);
+            txtBuscar.TabIndex = 11;
+            // 
+            // lblResultado
+            // 
+            lblResultado.AutoSize = true;
+            lblResultado.Font = new Font("Cambria", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblResultado.Location = new Point(179, 349);
+            lblResultado.Name = "lblResultado";
+            lblResultado.Size = new Size(82, 19);
+            lblResultado.TabIndex = 12;
+            lblResultado.Text = "Resultado:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Cambria", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(258, 274);
+            label2.Name = "label2";
+            label2.Size = new Size(237, 25);
+            label2.TabIndex = 13;
+            label2.Text = "Búsqueda de estudiantes";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(lblResultado);
+            Controls.Add(txtBuscar);
+            Controls.Add(lblBuscar);
+            Controls.Add(btnBuscar);
+            Controls.Add(label1);
             Controls.Add(lblPromedio);
             Controls.Add(lblEdad);
             Controls.Add(lbltxtNombre);
@@ -134,5 +212,11 @@
         private Label lbltxtNombre;
         private Label lblEdad;
         private Label lblPromedio;
+        private Label label1;
+        private Button btnBuscar;
+        private Label lblBuscar;
+        private TextBox txtBuscar;
+        private Label lblResultado;
+        private Label label2;
     }
 }
--- estudiante/RegistroE.cs
+++ estudiante/RegistroE.cs
@@ -1,26 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace estudiante
+﻿internal class RegistroE
 {
-    internal class RegistroE
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+    public double Promedio { get; set; }
+
+    public RegistroE(string nombre, int edad, double promedio)
     {
-        public string Nombre { get; set; }
-        public int Edad { get; set; }
-        public double Promedio { get; set; }
-       
-        public RegistroE(string nombre, int edad, double promedio)
+        ValidarNombre(nombre);
+        ValidarEdad(edad);
+        ValidarPromedio(promedio);
+
+        Nombre = nombre;
+        Edad = edad;
+        Promedio = promedio;
+    }
+
+    private void ValidarNombre(string nombre)
+    {
+        if (string.IsNullOrWhiteSpace(nombre) || nombre.Length < 3 || !nombre.All(c => char.IsLetter(c) || c == ' '))
         {
-            Nombre = nombre;
-            Edad = edad;
-            Promedio = promedio;
+            throw new Exception("El nombre debe contener solo letras y mínimo 3 caracteres.");
         }
-        public override string ToString()
+    }
+
+    private void ValidarEdad(int edad)
+    {
+        if (edad < 15 || edad > 100)
         {
-            return $"Nombre: {Nombre}, Edad: {Edad}, Promedio: {Promedio}";
+            throw new Exception("La edad debe estar entre 15 y 100.");
         }
+    }
+
+    private void ValidarPromedio(double promedio)
+    {
+        if (promedio < 0 || promedio > 10)
+        {
+            throw new Exception("El promedio debe estar entre 0 y 10.");
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"Nombre: {Nombre}, Edad: {Edad}, Promedio: {Promedio}";
     }
 }
